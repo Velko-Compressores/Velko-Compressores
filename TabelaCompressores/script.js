d8e76df56f64a9ed3e017aab328ebffb725b0598
Velko_Compressores <diego.rosa@velko.com.br>
2025-11-04 09:33:59 -0300

    update tabela compressores and style tabela compressores and script tabela compressores

--- TabelaCompressores/script.js
+++ TabelaCompressores/script.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
   const image = document.getElementById('tabela-img');
   const panzoom = Panzoom(image, {
-    maxScale: 5,
-    contain: 'outside'
+      minScale: 0.1,
+      maxScale: 1000,
+      contain: 'false',
+      canvas: true // permite arrastar livremente
   });
   image.parentElement.addEventListener('wheel', panzoom.zoomWithWheel);
 });
-
 function showInfo(part) {
   const info = {
     motor: "<strong>Motor Embutido</strong><br>Artigo: 1001<br>Peso: 12kg<br>Ano: 2020",
